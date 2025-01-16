95f62dafaead8a1342376aeefae32e1295961fca
MAGG <rassidan@gmail.com>
2025-01-16 05:21:56 -0600

    added fps

--- main.py
+++ main.py
@@ -8,7 +8,12 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+
     pygame.init()
+    dt = 0
+    clock = pygame.time.Clock()
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -19,6 +24,7 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
